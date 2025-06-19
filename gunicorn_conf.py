fc3054aa66be23676cd07f2ccf98101f9af37481
Srivaths P <srivathspradeep@gmail.com>
2025-06-20 02:29:37 +0530

    Minor fix

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,4 +1,4 @@
-def worker_init(worker):
+def post_worker_init(worker):
     print(f"Gunicorn worker {worker.pid}: Initializing and loading contest data.")
     from app.services.contest_service import load_server_data
     load_server_data()
